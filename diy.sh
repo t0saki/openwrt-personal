6a1fb64a1780142a5cdbd733a0fe08f0d72f61e9
Zhengxiao Wu <15712521+t0saki@users.noreply.github.com>
2025-01-03 18:55:31 +0800

    other mod

--- diy.sh
+++ diy.sh
@@ -9,3 +9,9 @@ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 make package/mosdns/luci-app-mosdns/compile V=s
 
+# change admin address
+sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
+
+# add date to build info
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
