ea2c050e1e1fb19dade515d21946124c5ee52e39
Zhengxiao Wu <15712521+t0saki@users.noreply.github.com>
2025-01-03 19:19:21 +0800

    remove build ipk

--- diy.sh
+++ diy.sh
@@ -1,13 +1,10 @@
 # add lucky
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-make package/lucky/lucky/compile V=s
-make package/lucky/luci-app-lucky/compile V=s
 
 # add mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-make package/mosdns/luci-app-mosdns/compile V=s
 
 # change admin address
 sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
