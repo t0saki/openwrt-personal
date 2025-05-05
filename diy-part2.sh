b8731618bc377b82d10386348de777a0bdddce5e
tosaki <tosaki.wzx@gmail.com>
2025-05-05 21:54:21 +0800

    Add new packages and custom scripts for build configuration
    - Updated alderlake.config and base.config to include additional packages such as libcurl, mbedtls, and various utilities.
    - Introduced diy-part1.sh and diy-part2.sh scripts for adding specific packages and modifying build configurations.
    - Updated run.sh to execute the new custom scripts during the build process.
    - Adjusted GitHub Actions workflow to run the new scripts as part of the build process.

---
+++ diy-part2.sh
@@ -0,0 +1,6 @@
+# change admin address
+sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
+
+# add date to build info
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' ./include/image.mk
+sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' ./include/image.mk
