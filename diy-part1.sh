956cea50c9e668820c9fb7722d60639e25a2e141
Zhengxiao Wu <15712521+t0saki@users.noreply.github.com>
2025-06-23 11:08:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,3 +5,8 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # add mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# add string to build info
+BUILD_DATE=$(date +'%Y%m%d')
+PERSONAL_ID="tosaki"
+echo "CONFIG_EXTRA_IMAGE_NAME=\"${EXTRA_NAME}\"" >> .config
