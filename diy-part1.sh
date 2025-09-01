46baf8d8a45d44b674a2f1742dcbc54c653b7bdc
tosaki <tosaki.wzx@gmail.com>
2025-09-02 04:59:48 +0800

    Update package versions in diy-part1.sh and tr3000_v1.config

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,8 @@
 # add lucky
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # add mosdns
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
