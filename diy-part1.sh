a760331496889ed9aae567fd2c56eaadf4ba752b
tosaki <tosaki.wzx@gmail.com>
2026-02-24 22:55:34 +0800

    feat: Upgrade to 25.12

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # add mosdns
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
